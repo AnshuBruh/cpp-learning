d17b3604434325f3ac4a8e8a0c301a30e5dd1e7a
AnshuBruh <anshumahour340@gmail.com>
2026-09-15 21:42:15 +0530

    Program2

---
+++ Iterative-statements/reversecounting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "Enter number : ";
+    cin >> num;
+
+    for(int i=20; i>=1; i--){
+        cout << i << endl;
+    }
+    return 0;
+}
